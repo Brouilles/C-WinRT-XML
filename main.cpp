aee00b7198f761a006fccf57ca83eb9096a00bc6
DEZEIRAUD Gaëtan <gaetan@dezeiraud.com>
2021-08-14 17:03:53 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::Storage;
+using namespace Windows::Data::Xml::Dom;
+
+int main()
+{
+    init_apartment();
+
+    StorageFile file = StorageFile::GetFileFromPathAsync(L"C:\\Users\\gaeta\\Works\\ReadXMLWinRT\\XMLFile.xml").get();
+
+    XmlLoadSettings settings = XmlLoadSettings();
+    settings.ElementContentWhiteSpace(false);
+
+	XmlDocument doc = XmlDocument::LoadFromFileAsync(file, settings).get();
+
+    XmlElement root = doc.DocumentElement();
+    XmlNodeList childNodes = root.ChildNodes();
+
+    printf("There are %d elements\n", childNodes.Size());
+	printf("====\n");
+    for (IXmlNode child : childNodes)
+    {
+        auto childs = child.ChildNodes();
+        if (childs.Size() > 0)
+        {
+            printf("Name: %ls\n", childs.Item(0).InnerText().c_str());
+            printf("Job: %ls\n", childs.Item(1).InnerText().c_str());
+        }
+        else
+            printf("INVALID\n");
+    	
+        printf("====\n");
+    }
+
+    return 0;
+}
